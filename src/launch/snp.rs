722071aa60b90fd83813968f4853c5f8e7795c08
Andreas Stührk <andy@yaxi.tech>
2024-02-22 21:17:36 +0100

    Remove enc region registration from `update_data`
    Doesn't make sense with v12 host patches

--- src/launch/snp.rs
+++ src/launch/snp.rs
@@ -86,8 +86,6 @@ impl<U: AsRawFd, V: AsRawFd> Launcher<Started, U, V> {
         let launch_update_data = LaunchUpdate::from(update);
         let mut cmd = Command::from(&self.sev, &launch_update_data);
 
-        KvmEncRegion::new(update.uaddr).register(&mut self.vm_fd)?;
-
         SNP_LAUNCH_UPDATE
             .ioctl(&mut self.vm_fd, &mut cmd)
             .map_err(|e| cmd.encapsulate(e))?;
